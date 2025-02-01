9714bb3933a3018257a4abf05ebe61ede0d8d82d
Dasarrathi <swain.jeeten@gmail.com>
2025-02-01 17:38:53 +0530

    adding new fiels

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for books project.
+URL configuration for books2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,15 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("world/", include("world.urls")),
+    path('admin/', admin.site.urls),
+    path('books/', include('world.urls'),name="world")
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- world/urls.py
+++ world/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for books project.
+URL configuration for books1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
